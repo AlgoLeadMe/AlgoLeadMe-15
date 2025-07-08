83d0bc369cd05d7194cbedb42365327c70901f2b
Seol-Munhyeok <abc1234568@naver.com>
2025-07-08 22:57:20 +0900

    2025-07-09

---
+++ Seol-Munhyeok/메뉴_리뉴얼.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+from itertools import combinations
+
+def solution(orders, course):
+    answer = []
+    
+    for c in course:
+        # 매번 count 초기화
+        count = defaultdict(int)
+        # c개에 해당하는 조합을 orders에서 뽑아서 개수를 저장
+        for order in orders:
+            keys = combinations(order, c)
+            for key in keys:
+                key = sorted(key)  # 메뉴 구성은 사전순으로 정렬
+                count[''.join(key)] += 1
+    
+        # 주문 횟수기준 내림차순 정렬
+        sorted_items = sorted(count.items(), key=lambda x:x[1], reverse=True)
+        if sorted_items:
+            max_value = sorted_items[0][1]
+        
+        for menu, cnt in sorted_items:
+            # 최소 2번 이상 주문된 조합만 포함
+            if not sorted_items or cnt <= 1 or cnt != max_value:
+                break
+            answer.append(menu)
+            max_value = cnt
+            
+    return sorted(answer)
