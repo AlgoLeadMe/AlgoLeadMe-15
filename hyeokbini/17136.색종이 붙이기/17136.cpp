b77acbf7fe2e1fff0fecfa1d5a18ab51deccfeec
고혁빈 <rhgurqls00@gmail.com>
2025-07-17 03:16:11 +0900

    주석 추가

--- hyeokbini/17136.색종이 붙이기/17136.cpp
+++ hyeokbini/17136.색종이 붙이기/17136.cpp
@@ -9,29 +9,29 @@ void func(int x, int y, int curans)
 {
     if(y == 10)
     {
-        ans = min(ans,curans);
+        ans = min(ans,curans); // 모든 행을 탐색했다면 답을 갱신
         return;
     }
     if(x == 10)
     {
-        func(0, y + 1, curans);
+        func(0, y + 1, curans); // 한 행을 탐색한다면 다음 행으로
         return;
     }
     if(paper[x][y] == 0)
     {
-        func(x + 1,y,curans);
+        func(x + 1,y,curans); // 0인 경우는 탐색하지 않음
         return;
     }
     for (int k = 5; k > 0; k--) // 색종이 크기(큰 것부터)
     {
         if (papercount[k] == 5)
         {
-            continue;
+            continue; // 5개 이상 쓴 색종이는 탐색하지 않음
         }
         bool canmake = true;
         if (x + k > 10 || y + k > 10)
         {
-            continue;
+            continue; // 범위를 벗어나는 색종이는 탐색하지 않음
         }
         for (int i = x; i < x + k; i++)
         {
@@ -49,7 +49,7 @@ void func(int x, int y, int curans)
             {
                 for (int j = y; j < y + k; j++)
                 {
-                    paper[i][j] = 0;
+                    paper[i][j] = 0; // 색종이 덮기
                 }
             }
             papercount[k]++;
@@ -58,7 +58,7 @@ void func(int x, int y, int curans)
             {
                 for (int j = y; j < y + k; j++)
                 {
-                    paper[i][j] = 1;
+                    paper[i][j] = 1; // 복원
                 }
             }
             papercount[k]--;
@@ -85,13 +85,13 @@ int main()
     }
     if (!hasone)
     {
-        cout << 0;
+        cout << 0; // 1이 없다면 답은 0
         return 0;
     }
     func(0, 0, 0);
     if (ans == INT_MAX)
     {
-        cout << -1;
+        cout << -1; // 답이 갱신되지 않았다면 만들 수 있는 경우의 수가 없음
     }
     else
     {
